1649dbba229c9d49c8ae0ca95d256c4b7a3769bc
JotaPe <jcamacho249@unab.edu.co>
2024-05-19 21:51:04 -0500

    [Add] Heart animation speed

---
+++ Assets/Heart/Animator/HeartAnimationSpeed.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class HeartAnimationSpeed : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        UIManager.Instance.FCM.onValueChanged.AddListener(delegate { SetSpeed(UIManager.Instance.FCM.value); });
+    }
+
+    public void SetSpeed(float FCM)
+    {
+       float speed = (1f/60f) * FCM;
+        animator.speed = speed;
+
+        Debug.Log("Speed: " + speed);
+    }
+}
